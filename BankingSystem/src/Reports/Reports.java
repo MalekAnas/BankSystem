6d6c6343d851efed06b80fdddce979d4cf58cf3c
Pyramid <malek.horani432@hotmail.com>
2018-11-18 23:13:20 +0100

    transactions reports

--- BankingSystem/src/Reports/Reports.java
+++ BankingSystem/src/Reports/Reports.java
@@ -18,8 +18,31 @@ public class Reports {
 	public static void listWithdrawls(ArrayList<Transaction> transactions) {
 		System.out.println("Account# \t\t+ Amount \t\t+ Type");
 		
-		transactions.forEach((t)->{
-			System.out.println(t.getAccountNumber() + "\t\t" + t.getAmount() +"\t\t" + t.getType());
+		transactions.forEach((t)-> {
+			if(t.getType().equals("Credit"))
+				System.out.println(t.getAccountNumber() + "\t\t " 
+						+ t.getAmount() + "\t\t" + t.getType());
 		});
 	}
+	
+	public static void transactionsSummary(ArrayList<Transaction> transactions) {
+		System.out.println("Account# \t\t+ Amount \t\t+ Type");
+		double withdrawals = 0.0;
+		double deposits = 0.0;
+		
+		for (Transaction t : transactions) {
+			if(t.getType().equals("Debit")) {
+				withdrawals += t.getAmount();
+			}else if(t.getType().equals("Credit")) {
+				deposits += t.getAmount();
+			}
+		}
+		
+		System.out.println("total withdrawals amount:" + withdrawals);
+		System.out.println("total deposits amount:" + deposits);
+
+	}
+	
+	
+	
 }
