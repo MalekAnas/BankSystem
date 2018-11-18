b8ea701b4be3b4a8f5ce07faee0f08c68381e0bf
Pyramid <malek.horani432@hotmail.com>
2018-11-18 20:37:26 +0100

    updated entry point
    Co-Authored-By: Pyramid <malek.horani432@hotmail..com>

--- BankingSystem/src/Default/EntryPoint.java
+++ BankingSystem/src/Default/EntryPoint.java
@@ -3,11 +3,16 @@ package Default;
 import java.util.LinkedList;
 import java.util.Scanner;
 
+import Accounts.Account;
 import Accounts.AccountsList;
+import Accounts.Checking;
+import Accounts.Savings;
 import Connections.Connection;
 import Customers.CustomersList;
 
 public class EntryPoint {
+	
+	Menu menu;
 	
 	public static void main(String[] args) {
 		
@@ -16,10 +21,35 @@ public class EntryPoint {
 		CustomersList clientList = new CustomersList();
 		LinkedList<Connection> connectionList = new LinkedList();
 		
-		
-		Menu menu = new Menu();
+		menu = new Menu();
 		menu.printFirstMenu();
 		menu.performActionFirstMenu();
+	}
+	
+	private static void createANewAccount() {
+
+		getNewCustomerInfo();
+		String ssn = getNewCustomerInfo();
+		System.out.println("Choose the type of the account (saving or checking): ");
+		System.out.println("1) Saving Account.");
+		System.out.println("2) Checking Account.");
+		menu.printCreateAccountMenu();
+		int choice = getUserChoice();
+
+		Account account = new Account();
+
+		if (choice == 1) {
+			account = new Savings();
+			myAccountList.addNewAccount(account);
+		} else if (choice == 2) {
+			account = new Checking();
+			myAccountList.addNewAccount(account);
+		}
+
+		System.out.println(myAccountList.toString());
+		printFirstMenu();
+		performActionFirstMenu();
+
 	}
 	
 }
